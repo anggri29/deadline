742d939cc17a23adbcf1b200af7f2244f4b4a232
Angelina <angry.anggri@gmail.com>
2023-07-13 14:52:45 +0300

    commit

---
+++ src/test/java/ru/netology/banklogin/data/DataHelper.java
@@ -0,0 +1,44 @@
+package ru.netology.banklogin.data;
+
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.util.Locale;
+
+public class DataHelper {
+    private DataHelper() {
+    }
+
+    private static Faker faker = new Faker(new Locale("en"));
+
+    public static AuthInfo getAuthInfoWithTestData() {
+        return new AuthInfo("vasya", "qwerty123");
+    }
+
+    private static String generateRandomLogin() {
+        return faker.name().username();
+    }
+
+    private static String generateRandomPassword() {
+        return faker.internet().password();
+    }
+
+    public static AuthInfo generateRandomUser() {
+        return new AuthInfo(generateRandomLogin(), generateRandomPassword());
+    }
+
+    public static VerificationCode generateRandomVerificationCode() {
+        return new VerificationCode(faker.numerify("######"));
+    }
+
+    @Value
+    public static class VerificationCode {
+        String code;
+    }
+
+    @Value
+    public static class AuthInfo {
+        String login;
+        String password;
+    }
+}
---
+++ src/test/java/ru/netology/banklogin/test/BankLoginTest.java
@@ -0,0 +1,50 @@
+package ru.netology.banklogin.test;
+
+import org.junit.jupiter.api.*;
+import ru.netology.banklogin.data.DataHelper;
+import ru.netology.banklogin.data.SQLHelper;
+import ru.netology.banklogin.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.banklogin.data.SQLHelper.cleanDatabase;
+
+public class BankLoginTest {
+
+    @AfterAll
+    static void teardown() {
+        cleanDatabase();
+    }
+
+    @Test
+    @DisplayName("Should successfully login to dashboard with exist login and password from sut test data")
+    void shouldSuccessfulLogin() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisiblity();
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+    }
+
+    @Test
+    @DisplayName("Should get error notification if user is not exist in base")
+    void shouldGetErrorNotificationIfLoginWithRandomUserWithoutAddingToBase() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.generateRandomUser();
+        loginPage.validLogin(authInfo);
+        loginPage.verifyErrorNotificationVisiblity();
+    }
+
+    @Test
+    @DisplayName("Should get error notification if login with exist in base and active user and random verification code")
+    void shouldGetErrorNotificationIfLoginWithExistUserAndRandomVerificationCode() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisiblity();
+        var verificationCode = DataHelper.generateRandomVerificationCode();
+        verificationPage.verify(verificationCode.getCode());
+        verificationPage.verifyErrorNotificationVisiblity();
+    }
+
+}
